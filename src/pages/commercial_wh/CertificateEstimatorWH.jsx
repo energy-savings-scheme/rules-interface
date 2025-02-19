177f7e57ca5a7b0503ba212c2dade36f5d1f31ca
Kusmayadi <kusmayadi@kororo.co>
2025-02-19 13:50:44 +0700

    [DG22-1966] Remove PRC copy from f16 certificate page

--- src/pages/commercial_wh/CertificateEstimatorWH.jsx
+++ src/pages/commercial_wh/CertificateEstimatorWH.jsx
@@ -9,6 +9,7 @@ import SpinnerFullscreen from 'components/layout/SpinnerFullscreen';
 import HeroBanner from 'nsw-ds-react/heroBanner/heroBanner';
 import Alert from 'nsw-ds-react/alert/alert';
 import { IS_DRUPAL_PAGES } from 'types/app_variables';
+import {WH1_F16_electric_PDRSAug24_PRC_calculation, WH1_F16_electric_PDRSAug24_ESC_calculation} from 'types/openfisca_variables';
 
 export default function CertificateEstimatorWH(props) {
   const { entities, variables, brands } = props;
@@ -340,8 +341,8 @@ export default function CertificateEstimatorWH(props) {
 
           {stepNumber === 2 && (
             <CertificateEstimatorLoadClausesWH
-              variableToLoad1={'WH1_F16_electric_PDRSAug24_PRC_calculation'}
-              variableToLoad2={'WH1_F16_electric_PDRSAug24_ESC_calculation'}
+              variableToLoad1={WH1_F16_electric_PDRSAug24_PRC_calculation}
+              variableToLoad2={WH1_F16_electric_PDRSAug24_ESC_calculation}
               annualEnergySavings={'WH1_F16_electric_PDRSAug24_energy_savings'}
               peakDemandReductionSavings={'WH1_F16_electric_PDRSAug24_peak_demand_annual_savings'}
               variables={variables}
@@ -406,8 +407,8 @@ export default function CertificateEstimatorWH(props) {
 
           {stepNumber === 3 && (
             <CertificateEstimatorLoadClausesWH
-              variableToLoad1={'WH1_F16_electric_PDRSAug24_PRC_calculation'}
-              variableToLoad2={'WH1_F16_electric_PDRSAug24_ESC_calculation'}
+              variableToLoad1={WH1_F16_electric_PDRSAug24_PRC_calculation}
+              variableToLoad2={WH1_F16_electric_PDRSAug24_ESC_calculation}
               annualEnergySavings={'WH1_F16_electric_PDRSAug24_energy_savings'}
               peakDemandReductionSavings={'WH1_F16_electric_PDRSAug24_peak_demand_annual_savings'}
               variables={variables}
