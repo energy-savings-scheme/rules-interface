ef00fae07d7515cf2eaa1da9f8e109df29c70577
Felix Cahaya <felix.cahaya@kororo.co>
2025-05-13 15:20:19 +0700

    [DG22-2335] Hide Unexpected Header

--- src/pages/commercial_ac/ActivityRequirementsAirCon.jsx
+++ src/pages/commercial_ac/ActivityRequirementsAirCon.jsx
@@ -170,7 +170,7 @@ export default function ActivityRequirementsCommercialAC(props) {
           </div>
         )}
 
-        {stepNumber === 2 && (
+        {!IS_DRUPAL_PAGES && stepNumber === 2 && (
           <div className="nsw-grid nsw-grid--spaced">
             <div className="nsw-col nsw-col-md-12">
               <h2 className="nsw-content-block__title">
