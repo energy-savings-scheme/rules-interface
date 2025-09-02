3def603735a1808b6cd4c8309a6b2d60d5380b21
Felix Cahaya <fcahaya@kororo.net>
2025-09-02 09:27:06 +0700

    [DG22-2441] change annual peak demand reduction unit from kWh to kW

--- src/pages/pool_pumps/CertificateEstimatorLoadClausesPP.jsx
+++ src/pages/pool_pumps/CertificateEstimatorLoadClausesPP.jsx
@@ -320,7 +320,7 @@ export default function CertificateEstimatorLoadClausesPP(props) {
                     {Math.floor(calculationResult) === 0
                       ? 0
                       : formatNumber(Math.round(peakDemandReductionSavingsNumber * 100) / 100)}
-                  </b> kWh
+                  </b> kW
                 </p>
                 <p>
                   If you are receiving an estimation of 0 certificates, the brand and model may not
