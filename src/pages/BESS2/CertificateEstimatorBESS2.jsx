fa4890c95fb66af9b4f0d9af110d5e545f98946e
felixc-kororo <felix.cahaya@kororo.co>
2025-10-23 13:41:41 +0700

    [DG22-2595] apply accessibility fixes for BESS2 * fix issue accessibility zoom content visibility * fix issue accessibility heading level * fix issue accessibility focus not managed when error displayed * fix issue accessibility related item not marked up in a list

--- src/pages/BESS2/CertificateEstimatorBESS2.jsx
+++ src/pages/BESS2/CertificateEstimatorBESS2.jsx
@@ -18,6 +18,7 @@ import {
   updateFeedbackFormAnalytics,
   clearSearchCaptureAnalytics,
 } from 'lib/analytics';
+import { focusElement } from 'lib/helper';
 import FeedbackComponent from 'components/feedback/feedback';
 import MoreOptionsCard from 'components/more-options-card/more-options-card';
 
@@ -109,6 +110,12 @@ export default function CertificateEstimatorBESS2(props) {
     fetchCertificatePrice()
   }, []);
 
+  useEffect(() => {
+    if (calculationError && calculationError2 && showError) {
+      focusElement("error-calculation");
+    }
+  }, [calculationError, calculationError2, showError])
+
   return (
     <Fragment>
       {!IS_DRUPAL_PAGES && (
@@ -126,7 +133,7 @@ export default function CertificateEstimatorBESS2(props) {
         </div>
       )}
 
-      <div className="nsw-container" style={{ paddingLeft: 0 }}>
+      <div className="nsw-container" style={{ paddingLeft: 0, paddingRight: 0 }}>
         <br></br>
         <br></br>
         {!IS_DRUPAL_PAGES && stepNumber !== 2 && (
@@ -164,13 +171,15 @@ export default function CertificateEstimatorBESS2(props) {
           </div>
         )}
 
-        <ProgressIndicator step={stepNumber} of={2} style={{ width: '80%', marginTop: '3rem' }} />
+        <ProgressIndicator step={stepNumber} of={2} style={{ marginTop: '3rem' }} className="nsw-col-lg-10" />
 
         {stepNumber === 2 && loading && !showError && <SpinnerFullscreen />}
 
         <Fragment>
           {stepNumber === 2 && calculationError && calculationError2 && showError && (
-            <Alert as="error" title="Sorry!" style={{ width: '80%' }}>
+            <Alert as="error" customTitle={
+              <h3 dangerouslySetInnerHTML={{__html: "Sorry!"}}/>
+            } id="error-calculation" className="nsw-col-lg-10" tabIndex="-1">
               <p>We are experiencing technical difficulties right now, please try again later.</p>
             </Alert>
           )}
