cf14cfb608e22048d3f28917bcb2afdb0e5d8f81
Felix Cahaya <fcahaya@kororo.net>
2025-11-25 14:10:39 +0700

    [DG22-2501] remove console.log

--- src/lib/config.js
+++ src/lib/config.js
@@ -3,8 +3,6 @@ if (process.env.REACT_APP_ANALYTICS_ENABLED) {
   analyticsEnabled = process.env.REACT_APP_ANALYTICS_ENABLED.toLowerCase() === 'true'
 }
 
-console.log(process.env)
-
 const config = {
   djangoApiBaseUrl: process.env.REACT_APP_API_BASE_URL,
   openfiscaApiBaseUrl: process.env.REACT_APP_OPENFISCA_BASE_URL,
