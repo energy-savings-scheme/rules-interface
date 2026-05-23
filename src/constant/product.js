bf55d5d03341205fb69acbca2576cd9526a7154c
felixc-kororo <felix.cahaya@kororo.co>
2026-05-23 17:47:25 +0700

    DG22-2766 add new constant air conditioner types

---
+++ src/constant/product.js
@@ -0,0 +1,18 @@
+export const AIR_CONDITIONER_TYPES = {
+  'ducted': {
+    'portable': 'Ducted unitary system',
+    'unitary double duct wall mounted': 'Ducted unitary system',
+    'window wall': 'Ducted unitary system',
+    'single split system': 'Ducted single split system',
+    'fixed': 'Ducted multi-split system',
+    'vrf': 'Ducted multi-split system',
+  },
+  'non ducted': {
+    'portable': 'Non-ducted unitary system',
+    'unitary double duct wall mounted': 'Non-ducted unitary system',
+    'window wall': 'Non-ducted unitary system',
+    'single split system': 'Non-ducted single split system',
+    'fixed': 'Non-ducted multi-split system',
+    'vrf': 'Non-ducted multi-split system',
+  }
+}
