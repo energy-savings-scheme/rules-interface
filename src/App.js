f52085498bcb8109da11a993c3f9698657360d0a
David Rudi <drudi@noreply.git.kororo.net>
2026-06-11 12:02:58 +0000

    DG22-2741 BESS3: Interface or activity eligibility requirements workflow. (#82)
    Co-authored-by: David Rudi <drudi@noreply.git.kororo.net>
    Co-committed-by: David Rudi <drudi@noreply.git.kororo.net>

--- src/App.js
+++ src/App.js
@@ -52,6 +52,7 @@ import SolarBatteryCertificatePage from 'pages/homepage/SolarBatteryCertificateP
 import CertificateEstimatorBESS1 from 'pages/BESS1/CertificateEstimatorBESS1';
 import ActivityRequirementsBESS2 from 'pages/BESS2/ActivityRequirementsBESS2';
 import CertificateEstimatorBESS2 from 'pages/BESS2/CertificateEstimatorBESS2';
+import ActivityRequirementsBESS3 from 'pages/BESS3/ActivityRequirementsBESS3';
 import CertificateEstimatorBESS3 from 'pages/BESS3/CertificateEstimator';
 import CertificateEstimatorWH2_test from 'pages/WH2_test/CertificateEstimatorWH';
 import ActivityRequirementsF17 from 'pages/commercial_wh_f17/ActivityRequirementsWaterHeater';
@@ -629,6 +630,17 @@ function App() {
         <Route path="/residential-solar-battery-demand-response-eligibility" exact>
           {!IS_DRUPAL_PAGES && <Breadcrumb />}
           <ActivityRequirementsBESS2
+            entities={entities}
+            variables={variables}
+            loading={loading}
+            setEntities={setEntities}
+            setVariables={setVariables}
+            setLoading={setLoading}
+          />
+        </Route>
+        <Route path="/residential-apartments-battery-eligibility" exact>
+          {!IS_DRUPAL_PAGES && <Breadcrumb />}
+          <ActivityRequirementsBESS3
             entities={entities}
             variables={variables}
             loading={loading}
---
+++ src/pages/BESS3/LoadClausesActReq.jsx
@@ -0,0 +1,210 @@
+import React, { Fragment, useEffect, useState } from 'react';
+
+// Import services
+import moment from 'moment';
+
+// Import components
+import CalculateBlock from 'components/calculate/CalculateBlock';
+
+import Button from 'nsw-ds-react/button/button';
+import OpenFiscaApi from 'services/openfisca_api';
+import Alert from 'nsw-ds-react/alert/alert';
+import SpinnerFullscreen from 'components/layout/SpinnerFullscreen';
+import { focusElement } from 'lib/helper';
+
+export default function LoadClausesBESS3(props) {
+  const {
+    variableToLoad,
+    variables,
+    entities,
+    setStepNumber,
+    stepNumber,
+    formValues,
+    setFormValues,
+    dependencies,
+    clausesForm,
+    setClausesForm,
+    showError,
+    setShowError,
+    onValidateUserType,
+  } = props;
+
+  const [variable, setVariable] = useState({}); // all info about variable
+
+  var today = new Date();
+  const [calculationDate, setCalculationDate] = useState(moment(today).format('YYYY-MM-DD'));
+
+  const [calculationResult, setCalculationResult] = useState(null);
+  const [calculationError, setCalculationError] = useState(false);
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (stepNumber === 1) {
+      setCalculationResult(null);
+    }
+  }, [stepNumber]);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  useEffect(() => {
+    OpenFiscaApi.getVariable(variableToLoad)
+      .then((res) => {
+        setVariable(res.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [variableToLoad]);
+
+  useEffect(() => {
+    if (calculationError && showError) {
+      focusElement('error-calculation');
+    }
+  }, [calculationError, showError]);
+
+  const formatResultString = (result) => {
+    if (typeof result === 'boolean') {
+      if (result === true) {
+        return 'you have met the activity eligibility requirements';
+      } else {
+        return 'you have NOT met the activity eligibility requirements';
+      }
+    }
+
+    return JSON.stringify(result) + ' kW';
+  };
+
+  const formatBooleanToString = (result) => {
+    return result === true ? 'Yes' : 'No';
+  };
+
+  if (!variable) return null;
+
+  return (
+    <div className style={{ marginBottom: '7%' }}>
+      <div>
+        {stepNumber === 1 && (
+          <Fragment>
+            <CalculateBlock
+              calculationDate={calculationDate}
+              variable={variable}
+              variables={variables}
+              entities={entities}
+              calculationResult={calculationResult}
+              setCalculationResult={setCalculationResult}
+              setCalculationError={setCalculationError}
+              stepNumber={stepNumber}
+              setStepNumber={setStepNumber}
+              formValues={formValues}
+              setFormValues={setFormValues}
+              backAction={(e) => {
+                setStepNumber(stepNumber - 1);
+              }}
+              dependencies={dependencies}
+              workflow={'eligibility'}
+              loading={loading}
+              setLoading={setLoading}
+              showError={showError}
+              setShowError={setShowError}
+              onValidateUserType={onValidateUserType}
+            />
+          </Fragment>
+        )}
+
+        {stepNumber === 2 && loading && !showError && <SpinnerFullscreen />}
+
+        {stepNumber === 2 && calculationError && showError && (
+          <Alert
+            as="error"
+            customTitle={<h3 dangerouslySetInnerHTML={{ __html: 'Sorry!' }} />}
+            id="error-calculation"
+            className="nsw-col-lg-10"
+            tabIndex="-1"
+          >
+            <p>We are experiencing technical difficulties right now, please try again later.</p>
+          </Alert>
+        )}
+
+        {stepNumber === 2 && calculationResult !== null && (
+          <Fragment>
+            {
+              <div style={{ marginTop: '5%' }}>
+                <Alert
+                  as="info"
+                  customTitle={<h3 dangerouslySetInnerHTML={{ __html: 'Activity Requirements' }} />}
+                  className="nsw-col-lg-10"
+                >
+                  <p data-ui-name="eligibility-result-text">
+                    {/* <h4 className="nsw-content-block__title" style={{ textAlign: 'center' }}> */}
+                    Based on the information you have provided{' '}
+                    {formatResultString(calculationResult)}.
+                  </p>
+                </Alert>
+
+                {calculationResult === false && (
+                  <Alert
+                    as="warning"
+                    customTitle={
+                      <h3
+                        dangerouslySetInnerHTML={{
+                          __html: 'The following answers were ineligible:',
+                        }}
+                      />
+                    }
+                    className="nsw-col-lg-10"
+                  >
+                    <p>
+                      {clausesForm.length > 0 &&
+                        clausesForm.map((item, i) => (
+                          <React.Fragment>
+                            <br></br>
+                            <br></br>
+                            <div class="nsw-global-alert__title">
+                              {item.metadata.display_question} :{' '}
+                              {formatBooleanToString(item.form_value)}
+                            </div>
+                            <p data-ui-name={item.name} style={{ whiteSpace: 'pre-line' }}>
+                              {item.metadata.eligibility_clause &&
+                                item.metadata.eligibility_clause.split('<br />').join('\n')}
+                            </p>
+                            <br></br>
+                          </React.Fragment>
+                        ))}
+                    </p>
+                  </Alert>
+                )}
+              </div>
+            }
+          </Fragment>
+        )}
+
+        {stepNumber === 2 && (
+          <Fragment>
+            <div className="nsw-row">
+              <div
+                className="nsw-col-md-9"
+                style={{ padding: 'inherit', marginTop: '5%', marginBottom: '5%' }}
+              >
+                <Button
+                  style={{ float: 'left' }}
+                  as="dark"
+                  data-ui-name="recalculate"
+                  onClick={(e) => {
+                    setClausesForm([]);
+                    setStepNumber(stepNumber - 1);
+                  }}
+                >
+                  Check activity requirements again
+                </Button>
+              </div>
+            </div>
+          </Fragment>
+        )}
+      </div>
+    </div>
+  );
+}
